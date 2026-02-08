879e8bd2d1fd5af3dd4e541cb431452f1621ca9a
Madhurika <madhurika@Madhurikas-MacBook-Air.local>
2026-02-09 00:58:20 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/madhurika/reliableOrderService/ReliableOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.madhurika.reliableOrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliableOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliableOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madhurika/reliableOrderService/ReliableOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhurika.reliableOrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliableOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
